19d5dae168745d62c052582d9495b726228cdf8e
Dilshodbek Ake <rajabovd808@gmail.com>
2022-10-02 09:56:13 +0300

    dwfwf

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React from "react";
+
+const App = () => {
+   return(
+    <div>
+          <div class="container">
+        <div class="col">
+            <div class="image">
+                <div class="icon">
+                    <p>hot</p>
+                    <p><i class="fa-solid fa-heart"></i>
+                        <i class="fa-solid fa-repeat"></i>
+                    </p>
+                </div>
+            </div>
+            <div class="text">
+            <p>home in merrick way</p>
+            <p>enchanting three bedroom, three bath home widthspacious one bedroom, one bath...</p>
+            <div>
+                <p>bedrooms</p>
+                <p>pathrooms</p>
+                <p>areas</p>
+            </div>
+            <p><small>for sale</small>
+            $ 540,000</p>
+        </div>
+        </div>
+        <div class="col">
+            <div class="image">
+                <div class="icon">
+                    <p>hot</p>
+                    <p><i class="fa-solid fa-heart"></i>
+                        <i class="fa-solid fa-repeat"></i>
+                    </p>
+                </div>
+            </div>
+            <div class="text">
+            <p>home in merrick way</p>
+            <p>enchanting three bedroom, three bath home widthspacious one bedroom, one bath...</p>
+            <div>
+                <p>bedrooms</p>
+                <p>pathrooms</p>
+                <p>areas</p>
+            </div>
+            <p><small>for sale</small>
+            $ 540,000</p>
+            </div>
+        </div>
+        <div class="col">
+            <div class="image">
+                <div class="icon">
+                    <p>hot</p>
+                    <p><i class="fa-solid fa-heart"></i>
+                        <i class="fa-solid fa-repeat"></i>
+                    </p>
+                </div>
+            </div>
+            <div class="text">
+            <p>home in merrick way</p>
+            <p>enchanting three bedroom, three bath home widthspacious one bedroom, one bath...</p>
+            <div>
+                <p>bedrooms</p>
+                <p>pathrooms</p>
+                <p>areas</p>
+            </div>
+            <p><small>for sale</small>
+            $ 540,000</p>
+            </div>
+        </div>
+    </div>
+    </div>
+   )
+  
+  
+
+}
+ 
+export  default App
